4fb0ba60a82a014975a735d610417424dd46b63c
Ivan Cherepanov <mail@av.codes>
2019-07-07 14:53:51 +0300

    feat: scrolling, added parallax to cards/logo

--- scrollbars.js
+++ scrollbars.js
@@ -1,1 +1,21 @@
-// Nothing to see here
+import simpleParallax from 'simple-parallax-js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new simpleParallax(document.querySelector('.logo'), {
+    orientation: 'down',
+    scale: 1.2,
+    overflow: true,
+    delay: 2
+  });
+
+  const cards = Array
+    .from(document.querySelectorAll('[card]'))
+    .forEach(card => card.complete = true);
+  
+  new simpleParallax(document.querySelectorAll('[card]'), {
+    orientation: 'down',
+    overflow: true,
+    scale: 1.1,
+    delay: 5
+  })
+});
